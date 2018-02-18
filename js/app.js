e31c62c0073f31e3cd18b07a5ca1dc47585275ab
alvean <alvean.ekman@gmail.com>
2018-02-18 18:50:33 +0100

    småfix

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@
 	 * END CONFIG VARIABLES
 	 */
 
-	function queryWeather2() {
+	function queryWeather() {
 		$.ajax({
 			type: 'GET',
 			url: 'http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/167747?apikey=PQ50CT1melGWDC45CBxrXvDmV3mxtDJS&metric=true',
@@ -26,28 +26,29 @@
 			var current = result[0];
 			var forecast = result[6];
 
-			populate2(current);
-			populate2(forecast);
+			populate(current);
+			populate(forecast);
 		});
 	}
 
 
 	function dummyQueryWeather() {
-		var jsonresult = '[{"DateTime":"2018-02-15T21:00:00+01:00","EpochDateTime":1518724800,"WeatherIcon":7,"IconPhrase":"Cloudy","IsDaylight":false,"Temperature":{"Value":1.1,"Unit":"C","UnitType":17},"PrecipitationProbability":47,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=1&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=1&hbhhour=21&unit=c&lang=en-us"},{"DateTime":"2018-02-15T22:00:00+01:00","EpochDateTime":1518728400,"WeatherIcon":7,"IconPhrase":"Cloudy","IsDaylight":false,"Temperature":{"Value":0.6,"Unit":"C","UnitType":17},"PrecipitationProbability":44,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=1&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=1&hbhhour=22&unit=c&lang=en-us"},{"DateTime":"2018-02-15T23:00:00+01:00","EpochDateTime":1518732000,"WeatherIcon":7,"IconPhrase":"Cloudy","IsDaylight":false,"Temperature":{"Value":1.1,"Unit":"C","UnitType":17},"PrecipitationProbability":48,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=1&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=1&hbhhour=23&unit=c&lang=en-us"},{"DateTime":"2018-02-16T00:00:00+01:00","EpochDateTime":1518735600,"WeatherIcon":18,"IconPhrase":"Rain","IsDaylight":false,"Temperature":{"Value":1.6,"Unit":"C","UnitType":17},"PrecipitationProbability":51,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=0&unit=c&lang=en-us"},{"DateTime":"2018-02-16T01:00:00+01:00","EpochDateTime":1518739200,"WeatherIcon":7,"IconPhrase":"Cloudy","IsDaylight":false,"Temperature":{"Value":1.8,"Unit":"C","UnitType":17},"PrecipitationProbability":25,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=1&unit=c&lang=en-us"},{"DateTime":"2018-02-16T02:00:00+01:00","EpochDateTime":1518742800,"WeatherIcon":38,"IconPhrase":"Mostly cloudy","IsDaylight":false,"Temperature":{"Value":2.3,"Unit":"C","UnitType":17},"PrecipitationProbability":20,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=2&unit=c&lang=en-us"},{"DateTime":"2018-02-16T03:00:00+01:00","EpochDateTime":1518746400,"WeatherIcon":36,"IconPhrase":"Intermittent clouds","IsDaylight":false,"Temperature":{"Value":2.4,"Unit":"C","UnitType":17},"PrecipitationProbability":20,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=3&unit=c&lang=en-us"},{"DateTime":"2018-02-16T04:00:00+01:00","EpochDateTime":1518750000,"WeatherIcon":36,"IconPhrase":"Intermittent clouds","IsDaylight":false,"Temperature":{"Value":2.5,"Unit":"C","UnitType":17},"PrecipitationProbability":20,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=4&unit=c&lang=en-us"},{"DateTime":"2018-02-16T05:00:00+01:00","EpochDateTime":1518753600,"WeatherIcon":35,"IconPhrase":"Partly cloudy","IsDaylight":false,"Temperature":{"Value":1.8,"Unit":"C","UnitType":17},"PrecipitationProbability":20,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=5&unit=c&lang=en-us"},{"DateTime":"2018-02-16T06:00:00+01:00","EpochDateTime":1518757200,"WeatherIcon":35,"IconPhrase":"Partly cloudy","IsDaylight":false,"Temperature":{"Value":2.1,"Unit":"C","UnitType":17},"PrecipitationProbability":13,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=6&unit=c&lang=en-us"},{"DateTime":"2018-02-16T07:00:00+01:00","EpochDateTime":1518760800,"WeatherIcon":33,"IconPhrase":"Clear","IsDaylight":false,"Temperature":{"Value":1.8,"Unit":"C","UnitType":17},"PrecipitationProbability":0,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=7&unit=c&lang=en-us"},{"DateTime":"2018-02-16T08:00:00+01:00","EpochDateTime":1518764400,"WeatherIcon":1,"IconPhrase":"Sunny","IsDaylight":true,"Temperature":{"Value":1.8,"Unit":"C","UnitType":17},"PrecipitationProbability":0,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=8&unit=c&lang=en-us"}]';
+		var jsonresult = '[{"DateTime":"2018-02-16T20:00:00+01:00","EpochDateTime":1518807600,"WeatherIcon":33,"IconPhrase":"Clear","IsDaylight":false,"Temperature":{"Value":1.1,"Unit":"C","UnitType":17},"PrecipitationProbability":0,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=1&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=1&hbhhour=20&unit=c&lang=en-us"},{"DateTime":"2018-02-16T21:00:00+01:00","EpochDateTime":1518811200,"WeatherIcon":33,"IconPhrase":"Clear","IsDaylight":false,"Temperature":{"Value":1.1,"Unit":"C","UnitType":17},"PrecipitationProbability":0,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=1&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=1&hbhhour=21&unit=c&lang=en-us"},{"DateTime":"2018-02-16T22:00:00+01:00","EpochDateTime":1518814800,"WeatherIcon":33,"IconPhrase":"Clear","IsDaylight":false,"Temperature":{"Value":0.6,"Unit":"C","UnitType":17},"PrecipitationProbability":0,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=1&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=1&hbhhour=22&unit=c&lang=en-us"},{"DateTime":"2018-02-16T23:00:00+01:00","EpochDateTime":1518818400,"WeatherIcon":35,"IconPhrase":"Partly cloudy","IsDaylight":false,"Temperature":{"Value":0.6,"Unit":"C","UnitType":17},"PrecipitationProbability":0,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=1&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=1&hbhhour=23&unit=c&lang=en-us"},{"DateTime":"2018-02-17T00:00:00+01:00","EpochDateTime":1518822000,"WeatherIcon":36,"IconPhrase":"Intermittent clouds","IsDaylight":false,"Temperature":{"Value":0.0,"Unit":"C","UnitType":17},"PrecipitationProbability":0,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=0&unit=c&lang=en-us"},{"DateTime":"2018-02-17T01:00:00+01:00","EpochDateTime":1518825600,"WeatherIcon":7,"IconPhrase":"Cloudy","IsDaylight":false,"Temperature":{"Value":0.0,"Unit":"C","UnitType":17},"PrecipitationProbability":1,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=1&unit=c&lang=en-us"},{"DateTime":"2018-02-17T02:00:00+01:00","EpochDateTime":1518829200,"WeatherIcon":7,"IconPhrase":"Cloudy","IsDaylight":false,"Temperature":{"Value":0.0,"Unit":"C","UnitType":17},"PrecipitationProbability":1,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=2&unit=c&lang=en-us"},{"DateTime":"2018-02-17T03:00:00+01:00","EpochDateTime":1518832800,"WeatherIcon":7,"IconPhrase":"Cloudy","IsDaylight":false,"Temperature":{"Value":-0.6,"Unit":"C","UnitType":17},"PrecipitationProbability":1,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=3&unit=c&lang=en-us"},{"DateTime":"2018-02-17T04:00:00+01:00","EpochDateTime":1518836400,"WeatherIcon":7,"IconPhrase":"Cloudy","IsDaylight":false,"Temperature":{"Value":-0.6,"Unit":"C","UnitType":17},"PrecipitationProbability":1,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=4&unit=c&lang=en-us"},{"DateTime":"2018-02-17T05:00:00+01:00","EpochDateTime":1518840000,"WeatherIcon":7,"IconPhrase":"Cloudy","IsDaylight":false,"Temperature":{"Value":-0.6,"Unit":"C","UnitType":17},"PrecipitationProbability":1,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=5&unit=c&lang=en-us"},{"DateTime":"2018-02-17T06:00:00+01:00","EpochDateTime":1518843600,"WeatherIcon":7,"IconPhrase":"Cloudy","IsDaylight":false,"Temperature":{"Value":-0.6,"Unit":"C","UnitType":17},"PrecipitationProbability":3,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=6&unit=c&lang=en-us"},{"DateTime":"2018-02-17T07:00:00+01:00","EpochDateTime":1518847200,"WeatherIcon":36,"IconPhrase":"Intermittent clouds","IsDaylight":false,"Temperature":{"Value":-0.6,"Unit":"C","UnitType":17},"PrecipitationProbability":7,"MobileLink":"http://m.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&unit=c&lang=en-us","Link":"http://www.accuweather.com/en/de/friedrichshain/10243/hourly-weather-forecast/167747?day=2&hbhhour=7&unit=c&lang=en-us"}]'; 
 		var result = JSON.parse(jsonresult);
 
 		var current = result[0];
 		var forecast = result[5];
 
-		populate2(current, "current");
-		populate2(forecast, "forecast");
+		populate(current, "current");
+		populate(forecast, "forecast");
 	}
 
 
-	function populate2(current, div) {
+	function populate(current, div) {
 		var icon = $('#' + div + ' .icon');
 		var desc = $('#' + div + ' .desc');
 		var temp = $('#' + div + ' .temp');
+		var feeltemp = $('#' + div + ' .feeltemp');
 
 
 		if (icon.length)
@@ -58,6 +59,9 @@
 
 		if (temp.length)
 			temp.html(current.Temperature.Value);
+
+		if (feeltemp.length)
+			feeltemp.html(current.RealFeelTemperature.Value);
 	}
 
 
@@ -70,7 +74,7 @@
 		if ($('#date').length)
 			$('#date').html(moment().format('dddd, MMM Do'));
 
-		//queryWeather2();
+		//queryWeather();
 		dummyQueryWeather();
 
 		setInterval(function() {
@@ -82,7 +86,7 @@
 		}, 1000);
 
 		setInterval(function() {
-			//queryWeather2();
+			//queryWeather();
 			dummyQueryWeather();
 		}, waitBetweenWeatherQueries);
 
